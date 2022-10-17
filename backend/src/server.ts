e56f9648885a7a4a2cc7b97a8c07c5cda0dbde40
haseebullahabbasi <haseebabbasi00@gmail.com>
2022-10-17 13:16:04 +0500

    access token with expiry session

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,10 +4,11 @@ import connect from "./utils/connect";
 import logger from "./utils/logger";
 import routes from "./routes/routes";
 
+import deserializeUser from "./middleware/deserializeUser";
 
 const app = express()
 app.use(express.json())
-
+app.use(deserializeUser)
 const port = config.get<number>('port');
 
 app.listen(port, async ()=>
