a4c16fd3d096f1a477551ec01ac9c160aab0d75c
Haseeb Ullah <haseebabbasi00@gmail.com>
2022-10-26 16:57:38 +0500

    header with main compoent

--- frontend/my-app/src/components/GetStart.tsx
+++ frontend/my-app/src/components/GetStart.tsx
@@ -1,10 +1,29 @@
 import React, { FC } from 'react';
+import { arrowUp } from '../assets/assets';
+import styles from '../styles';
 
 const GetStart: FC = () => {
   return (
-    <React.Fragment>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] bg-blue-gradient rounded-full p-[2px] cursor-pointer`}>
+      <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
+            <span className='text-gradient'>
+              Get
 
-    </React.Fragment>
+            </span>
+          </p>
+          <img src={arrowUp} alt="arrow" className='object-contain w-[23px] h-[23px]' />
+
+        </div>
+        <p className='font-poppins font-medium text-[18px] leading-[23px]'>
+          <span className='text-gradient'>
+            Started
+          </span>
+        </p>
+
+      </div>
+    </div>
   )
 }
 export default GetStart;
